027b67e84d5765d1c8a406f1868b9887dd2b6658
Roman Iovlev <roman_iovlev@epam.com>
2020-02-22 13:43:31 +0300

    1.1.16

--- src/test/java/org/mytests/tests/TestsInit.java
+++ src/test/java/org/mytests/tests/TestsInit.java
@@ -1,12 +1,13 @@
 package org.mytests.tests;
 
-import com.epam.jdi.light.ui.html.HtmlSettings;
+import com.epam.jdi.light.elements.interfaces.complex.IsCombobox;
+import com.epam.jdi.light.ui.html.elements.complex.DataListOptions;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.mytests.uiobjects.example.site.SiteJdi;
 
+import static com.epam.jdi.light.elements.init.InitActions.INTERFACES;
 import static com.epam.jdi.light.elements.init.PageFactory.initElements;
-import static com.epam.jdi.light.logger.LogLevels.STEP;
 import static com.epam.jdi.light.settings.WebSettings.logger;
 import static org.mytests.uiobjects.example.site.SiteJdi.homePage;
 
@@ -15,8 +16,7 @@ public class TestsInit {
     @BeforeAll
     public static void setUp() {
         if (initialized)  return;
-        logger.setLogLevel(STEP);
-        HtmlSettings.init();
+        INTERFACES.update(IsCombobox.class, DataListOptions.class);
         initElements(SiteJdi.class);
         homePage.open();
         logger.info("Run Tests");

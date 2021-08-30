4ee3bd854638a1d42e3cc7a0a205c2cdf97d565b
n-ton <a.ostrenko@gmail.com>
2021-08-30 17:48:18 +0300

    issue-3250: tests fixes and parallel execution

--- src/test/java/org/mytests/tests/example/BaseTest.java
+++ src/test/java/org/mytests/tests/example/BaseTest.java
@@ -1,9 +1,25 @@
 package org.mytests.tests.example;
 
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mytests.tests.example.extensions.JDISetupExtension;
+import com.epam.jdi.light.driver.WebDriverFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.mytests.uiobjects.example.site.SiteJdi;
 
-@ExtendWith(JDISetupExtension.class)
+import static com.epam.jdi.light.elements.composite.WebPage.openSite;
+import static com.epam.jdi.light.settings.WebSettings.logger;
+
 public class BaseTest {
+
+    @BeforeEach()
+    protected void setUp() {
+        logger.info("Running test");
+        openSite(SiteJdi.class);
+    }
+
+    @AfterEach()
+    protected void tearDown() {
+        logger.info("Completing test");
+        WebDriverFactory.quit();
+    }
 
 }
